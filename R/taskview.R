f10efc4b7ce8c241b35623d01d487edd7621d7ef
Emi Tanaka <emi.tanaka@monash.edu>
2022-03-16 20:00:04 +1100

    add ctv_table, rename ctv_topics to ctv_names

--- R/taskview.R
+++ R/taskview.R
@@ -5,7 +5,7 @@
 #' @param repos A base URL of the repository. By default, `getOption("repos")` is tried
 #'  and otherwise `getOption("CRAN")` is used.
 #' @export
-ctv_pkgs <- function(topic = ctv_topics(), repos = NULL) {
+ctv_pkgs <- function(name = ctv_names(), repos = NULL) {
   topic <- match.arg(topic)
   w <- match(topic, ctv_topics())
   query <- ctv::available.views(repos = repos)[w]
@@ -16,6 +16,22 @@ ctv_pkgs <- function(topic = ctv_topics(), repos = NULL) {
 #' 
 #' @source https://cran.r-project.org/web/views/
 #' @export
-ctv_topics <- function() {
+ctv_names <- function() {
   names(ctv::available.views())
+}
+
+#' Get the CRAN task view as a table 
+#' 
+#' @inheritParams ctv_pkgs
+#' @export
+ctv_table <- function(repos = NULL) {
+  ctv_list <- ctv::available.views(repos = repos)
+  res <- lapply(ctv_list, function(x) data.frame(name = x$name, 
+                                              topic = x$topic, 
+                                              maintainer = x$maintainer, 
+                                              email = x$email, 
+                                              version = x$version, 
+                                              packages = I(list(x$packagelist$name)), 
+                                              repository = x$repository))
+  do.call(rbind, res)
 }
