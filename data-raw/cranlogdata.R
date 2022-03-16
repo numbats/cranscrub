9a82b9663e97d5ee412f8d51da3b29576ad492a8
Emi Tanaka <emi.tanaka@monash.edu>
2022-03-16 20:56:08 +1100

    cranlogdata added but requires further reduction in size

---
+++ data-raw/cranlogdata.R
@@ -0,0 +1,12 @@
+## code to prepare `cranlogdata` dataset goes here
+
+path <- here::here("data-raw", "cranlogs_processed/")
+fns <- dir(path)
+dl <- lapply(fns[grepl("^20(20)", fns)], function(afile) read.csv(gzfile(file.path(path, afile))))
+
+# Only 5 mb of data for CRAN 
+# Current one goes to 117 MB. Need to reduce. \
+# 2020 alone is 25 MB
+
+cranlogdata <- dplyr::bind_rows(!!!dl) 
+usethis::use_data(cranlogdata, overwrite = TRUE)
