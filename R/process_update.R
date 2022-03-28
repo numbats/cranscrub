f25cacf25ac122b2bc4c379a270ae2fb864a56e9
danyangdai <danyan.dai01@gmail.com>
2022-03-28 22:55:12 +1100

    reprocessing data including file date

--- R/process_update.R
+++ R/process_update.R
@@ -1,30 +1,28 @@
 
-# This is the script for updating the cranlog file to 1st of March
-# Process all the cranlog data on the server 
-
-start <- as.Date('2021-08-28')
-today <- as.Date('2022-03-01')
+start <- as.Date('2021-08-23')
+today <- as.Date('2021-08-27')
 all_days <- seq(start, today, by = 'day')
 year <- as.POSIXlt(all_days)$year + 1900
 urls <- paste0('http://cran-logs.rstudio.com/', year, '/', all_days, '.csv.gz')
 writeLines(urls, "pkg_cranlogs.txt")
 
+# Use wget to obtain files with short random delay
+# system("wget -i pkg_cranlogs.txt -w 2s -P cranlogs/")
 
-destfile <- paste0(all_days, '.csv.gz')
-
-n <- length(destfile)
-
-for (i in c(1:n)){
-  download.file(url = urls[i],destfile = destfile[i])
-}
-
-paths <- list.files("cranlog_new", pattern = "\\.csv\\.gz$", full.names = TRUE)
-
-done_paths <- list.files("cranlogs_processed/", pattern = "\\.csv\\.gz$")
-paths <- paths[!(basename(paths) %in% done_paths)]
-
+# Process data into a smaller more suitable format for analysis
+# Reduces 1 row per package download to become 1 row per package per day
+# 
+# Attempts to distinguish between duplicate downloads using metadata to give
+# unique downloads, which is comparable to the total downloads typically
+# reported.
 library(tidyverse)
 library(progressr)
+library(xfun)
+paths <- list.files("cranlogs/", pattern = "\\.csv\\.gz$", full.names = TRUE)
+
+# Remove already processed files
+done_paths <- list.files("cranlogs/processed/", pattern = "\\.csv$")
+paths <- paths[!(sans_ext(basename(paths)) %in% done_paths)]
 
 process_data <- function(paths) {
   p <- progressor(along = paths)
@@ -43,9 +41,9 @@ process_data <- function(paths) {
                ip_id = col_double()
              )) %>% 
       count(date, r_version, r_arch, r_os, package, version, country, ip_id) %>% 
-      group_by(date, package) %>% 
+      group_by(file_date = as.Date(substr(basename(file), 1, 10)), date, package) %>% 
       summarise(n_unique = n(), n_total = sum(n), .groups = "drop") %>% 
-      write_csv(file.path(dirname(file), basename(file)))
+      write_csv(file.path(dirname(file), "processed", basename(file)))
     gc()
     p(xfun::sans_ext(basename(file)))
   }
@@ -55,12 +53,10 @@ process_data <- function(paths) {
 handlers("progress")
 with_progress(process_data(paths))
 
-
 cran_new <- list.files(path="cranlogs_processed/processed", pattern = "\\.csv\\.gz$",full.names = TRUE) %>% 
   lapply(read_csv) %>% 
   bind_rows 
 
 saveRDS(cran_new, file = "processed_new.rds")
 
-saveRDS(cran_new, file = "processed_new.rds")
 
